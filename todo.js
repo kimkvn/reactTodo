4a8639932511b1f760df35b96d68e81af22169f0
kimkvn <kimkvn@gmail.com>
2016-07-21 18:20:51 -0400

    added jquery just in case I wanna check something. successfully bound an onChange handler! that fires a setState

--- todo.js
+++ todo.js
@@ -1,8 +1,37 @@
 
 var ListItem = React.createClass({
+  getInitialState: function(){
+    return{
+      task: ''
+    }
+  },
+
+  handleTaskChange: function(text){
+    this.setState({
+      task: text.target.value
+    });
+
+  },
+
+  handleTextCopy: function(){
+
+  },
+
   render: function(){
     return(
-      <h1>Hello</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Learn React..."
+          value={this.state.task}
+          onChange = {this.handleTaskChange}
+        />
+        <input type="submit"/>
+        <textarea
+          value="Duplicates go here"
+          onChange={this.handleTextCopy}
+        />
+      </div>
     );
   }
 });
