00721cf4af01558df3ba9ed184ba60bfebca10e4
kimkvn <kimkvn@gmail.com>
2016-07-21 17:56:44 -0400

    First steps! hooked up a separate JS(x) file because I really dislike writing js in the html file. Successfully called a simple render functioingit add todo.jsgit add todo.js

---
+++ todo.js
@@ -0,0 +1,13 @@
+
+var ListItem = React.createClass({
+  render: function(){
+    return(
+      <h1>Hello</h1>
+    );
+  }
+});
+
+ReactDOM.render(
+  <ListItem />,
+  document.getElementById('content')
+);
